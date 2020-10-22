f5d8bec6a59fb81977d6f1adec360a67f4eb798a
canicjusz <janek.bananek371@gmail.com>
2020-10-22 16:49:46 +0200

    little fix

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+    <nav class="nav">
+      <ul class="nav__list">
+        <li class="nav__item" goTo=".Accounts"><a class="nav__anchor" href="#accounts">Accounts</a></li>
+        <li class="nav__item" goTo=".Commends"><a class="nav__anchor" href="#commends">CS:GO commends</a></li>
+        <li class="nav__item" goTo=".Comments"><a class="nav__anchor" href="#comments">Steam comments</a></li>
+        <li class="nav__item" goTo=".Contact"><a class="nav__anchor" href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+    <main class="main">
+      <section class="section main-page">
+        <div class="section__text">
+          <h1 class="section__title"><a :href="contact.steam" class="section__anchor">vAsketti</a>service!</h1>
+          <h2 class="section__title-sub">Stop overpaying for accounts, commends and comments.</h2>
+        </div>
+      </section>
+      <section class="section Accounts">
+        <h1 class="section__title">Accounts</h1>
+        <account v-for="(obj, i) in accounts" :key="i" :content="obj"></account>
+      </section>
+      <sectionWithTable v-for="(obj, name) in sections" :key="name" :content="obj" :title="name"></sectionWithTable>
+      <section class="section Contact">
+        <div class="section__text">
+          <h1 class="section__title">Contact</h1>
+          <h1 class="section__title">Steam: <a :href="contact.steam" class="section__anchor">vAsketti</a>,</h1>
+          <h1 class="section__title">Discord: {{contact.discord}}</h1>
+        </div>
+      </section>
+    </main>
+    <background></background>
+    <footer class="footer">
+      <p class="footer__paragraph">This site was created by <a href="https://canicjusz.github.io">Canicjusz</a>.</p>
+    </footer>
+  </div>
+</template>
+
+<script>
+import sectionWithTable from "@/components/section.vue"
+import account from "@/components/account.vue"
+import background from "@/components/background.vue"
+import data from "@/data/accounts.json"
+import('@/helpers/scroller.js')
+
+export default {
+  name: 'App',
+  components: {
+    sectionWithTable,
+    background,
+    account
+  },
+  setup(){
+    const {contact, accounts, sections} = data
+    return {contact, accounts, sections}
+  }
+}
+</script>
+
+<style lang="sass">
+@import "@/styles/style.sass"
+</style>
---
+++ src/components/background.vue
@@ -0,0 +1,80 @@
+<template>
+    <audio :src="audio" @loadeddata="loaded.audio = true" loop class="audio" ref="audio"></audio>
+    <div class="video-container">
+        <video loop muted class="video" @loadeddata="loaded.video = true" ref="video">
+            <source :src="video"/>
+            Your browser does not support the video tag.
+        </video>
+    </div>
+    <teleport to="#app">
+        <div class="filter" v-if="!clicked">
+            <div class="popup">
+                <div class="popup__title">
+                    <p class="popup__title-text">vAskettiservice!</p>
+                    <div class="popup__title-close">
+                        <span class="close-line close-line-left"></span>
+                        <span class="close-line close-line-right"></span>
+                    </div>
+                </div>
+                <div class="popup__content">
+                    <img :src="icon" alt="" class="icon">
+                    <p class="popup__text">Website status:<br> {{ text }}</p>
+                </div>
+                <div class="popup__button-container">
+                    <button :disabled="!(loaded.audio && loaded.video)" :class="['popup__button', disable]" @click="playThings" >Ok</button>
+                </div>
+            </div>
+        </div>
+    </teleport>
+</template>
+
+<script>
+import video from "../background/filmik.webm"
+import audio from "../background/muzyka.mp3"
+import icon from "../icon/win95iconnetwork.png"
+export default {
+    data() {
+        return {
+            video,
+            audio,
+            icon,
+            clicked: false,
+            loaded: {
+                video: false,
+                audio: false,
+            }
+        }
+    },
+    computed: {
+        text(){
+            return this.loaded.audio && this.loaded.video ? 'Website loaded' : 'Loading the website'
+        },
+        disable(){
+            return this.loaded.audio && this.loaded.video ? 'popup__button--enabled' : 'popup__button--disabled'
+        }
+    },
+    methods: {
+        playThings(){
+            this.clicked = true
+            this.$refs.video.play()
+            this.$refs.audio.play()
+        }
+    },
+    mounted(){
+        document.addEventListener('visibilitychange', ()=>{
+            //methods doesnt work even if i bind "this" object, so gotta copy and paste
+            if(document.visibilityState === 'visible' && this.clicked){
+                this.$refs.video.play()
+                this.$refs.audio.play()
+            }else{
+                this.$refs.video.pause()
+                this.$refs.audio.pause()
+            }
+        })
+    }
+}
+</script>
+
+<style>
+
+</style>
